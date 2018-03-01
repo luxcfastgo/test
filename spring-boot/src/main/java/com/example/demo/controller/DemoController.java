d74ba8a5a047f6e17795cee9cb7e95401e1730b3
卢贤超 <luxc@proem.cn>
2018-03-01 15:30:37 +0800

    ssss

---
+++ spring-boot/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@RequestMapping("/get")
+	public String demo(String msg) {
+		return "你好" + msg;
+	}
+
+}
